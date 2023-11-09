a71f645d3b576ef03dd423042d6d754feb1939a1
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-09 16:52:47 -0300

    criado novo arquivo index.js para gerenciar o conteúdo da página

---
+++ index.js
@@ -0,0 +1,4 @@
+import updateWorkspace from "./source/workspace.js";
+
+//mostra o conteúdo do workspace atualizado na tela
+updateWorkspace()
--- source/workspace.js
+++ source/workspace.js
@@ -2,6 +2,8 @@ import createBoard from "./board.js"
 import boardData from "./database.js"
 
 // cria todos os boards no workspace
-boardData.map((boardData) => createBoard(boardData))
+const updateWorkspace = () => {
+    boardData.map((boardData) => createBoard(boardData))
+}
 
-
+export default updateWorkspace;
